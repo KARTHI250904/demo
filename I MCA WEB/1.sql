e653c3a7732b92fcfc9041b0c39b5c3bee744099
KARTHI250904 <karthickkumar25092004@gmail.com>
2025-12-29 00:36:47 +0530

    Update 1.sql

--- I MCA WEB/1.sql
+++ I MCA WEB/1.sql
@@ -1,1 +1,9 @@
-select * from cricket;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(255)
+);
+
+INSERT INTO users (username, email, password)
+VALUES ('admin', 'admin@gmail.com', MD5('123456'));
